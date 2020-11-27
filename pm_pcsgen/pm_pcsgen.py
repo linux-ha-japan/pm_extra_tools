96052c8369301f9ee4f4ad5c88e0d6b33676bcba
Kazunori INOUE <kazunori_inoue@newson.co.jp>
2020-11-27 12:35:58 +0900

    pm_pcsgen: fix the filter function

--- pm_pcsgen/pm_pcsgen.py
+++ pm_pcsgen/pm_pcsgen.py
@@ -1694,21 +1694,29 @@ class Gen:
       if p.returncode != 0:
         error(p.stderr.decode(U8) if p.stderr else p.stdout.decode(U8))
         return False
-      for (k,x) in self.filter.items():
-        c = x.get('targetcommand',None)
-        if c and re.search(r'%s'%(re.sub('\'','\\\'',c)),cmd) is not None:
-          i,d = x.get('filtertoinfo',None),x.get('filtertodebug',None)
-          if i and re.search(r'%s'%(re.sub('\'','\\\'',i)),p.stdout.decode(U8)) is not None:
-            log.debug(f'[{k}]の設定({CONF})によりpcsのメッセージをフィルタリングします')
-            info(p.stdout.decode(U8),x.get('filterreason',None))
-            return True
-          elif d and re.search(r'%s'%(re.sub('\'','\\\'',d)),p.stdout.decode(U8)) is not None:
-            debug(p.stdout.decode(U8),x.get('filterreason',None))
-            return True
-      if re.match(r'warning: ',p.stdout.decode(U8),flags=re.I) is not None:
-        warn(p.stdout.decode(U8))
-      elif p.stdout:
-        info(p.stdout.decode(U8))
+      f,n = [],[]
+      for s in p.stdout.decode(U8).splitlines():
+        for (k,x) in self.filter.items():
+          c = x.get('targetcommand',None)
+          if c and re.search(r'%s'%(re.sub('\'','\\\'',c)),cmd) is not None:
+            i,d = x.get('filtertoinfo',None),x.get('filtertodebug',None)
+            if (i and re.search(r'%s'%(re.sub('\'','\\\'',i)),s) is not None or
+                d and re.search(r'%s'%(re.sub('\'','\\\'',d)),s) is not None):
+              f.append((k,s))
+              break
+        else:
+          n.append(s)
+      for (k,x) in f:
+        if self.filter[k].get('filtertoinfo',None):
+          log.debug(f'[{k}]の設定({CONF})によりpcsのメッセージをフィルタリングします')
+          info(x,self.filter[k].get('filterreason',None))
+        else:
+          debug(x,self.filter[k].get('filterreason',None))
+      x = '\n'.join(n)
+      if re.match(r'warning: ',x,flags=re.I) is not None:
+        warn(x)
+      elif x:
+        info(x)
       return True
     except Exception as e:
       log.innererr('CIBファイルの出力に失敗しました',e)
